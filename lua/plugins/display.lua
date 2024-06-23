3d4ca0ca96958eb5c7eddef689d286d10bf8724a
Justin Liang <justinliang1020@gmail.com>
2024-06-23 10:34:33 -0400

    remove custom name since it broke

--- lua/plugins/display.lua
+++ lua/plugins/display.lua
@@ -16,23 +16,4 @@ return {
       background_colour = "#000000",
     },
   },
-  {
-    "goolord/alpha-nvim",
-    opts = function(_, opts)
-      local logo = [[
-                     __                   
- __                 /\ \__  __            
-/\_\  __  __    ____\ \ ,_\/\_\    ___    
-\/\ \/\ \/\ \  /',__\\ \ \/\/\ \ /' _ `\  
- \ \ \ \ \_\ \/\__, `\\ \ \_\ \ \/\ \/\ \ 
- _\ \ \ \____/\/\____/ \ \__\\ \_\ \_\ \_\
-/\ \_\ \/___/  \/___/   \/__/ \/_/\/_/\/_/
-\ \____/                                  
- \/___/
-
-@justinliang1020
-      ]]
-      opts.section.header.val = vim.split(logo, "\n", { trimempty = true })
-    end,
-  },
 }
