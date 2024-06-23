44216606361e460b332c21f8b2f81e6e77485700
Justin Liang <justinliang1020@gmail.com>
2024-06-22 20:51:46 -0400

    initial commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      colorscheme = "gruvbox",
+      transparent = "true",
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      background_colour = "#000000",
+    },
+  },
+}
