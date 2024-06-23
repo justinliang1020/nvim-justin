d9b28a66e354197b0e2ee71887d7e397ce20f9ff
Justin Liang <justinliang1020@gmail.com>
2024-06-23 16:44:04 -0400

    disable transparency on floats and sidebars

--- lua/plugins/display.lua
+++ lua/plugins/display.lua
@@ -2,11 +2,10 @@ return {
   {
     "folke/tokyonight.nvim",
     opts = {
-      -- colorscheme = "gruvbox",
       transparent = "true",
       styles = {
-        sidebars = "transparent",
-        floats = "transparent",
+        sidebars = "normal",
+        floats = "normal",
       },
     },
   },
