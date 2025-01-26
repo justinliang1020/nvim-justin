5dd78e45ba8b21ae8edc3b12c68ccd4134b93f29
Justin Liang <justinliang1020@gmail.com>
2025-01-25 23:15:43 -0500

    fix broken cd show diagnostics keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+-- For some reason line diagnostics keymap wasn't showing up in a typescript file when pressing this keyybinding after updating lazyvim version
+-- even though it is in the default keymap https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+map("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
